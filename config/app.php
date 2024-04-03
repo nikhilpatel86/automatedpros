f3da7716e963b819ff74c320dabc03795318b5e4
nikhil patel <nikhil.07patel@gmail.com>
2024-04-04 01:50:25 +0530

    AJAX Signup with DataTableGrid Done

--- config/app.php
+++ config/app.php
@@ -122,13 +122,5 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
- 
-    // 'providers' => [        
-    //     Yajra\DataTables\DataTablesServiceProvider::class,
-    // ],
-
-    // 'aliases' => [ 
-    //     'DataTables' => Yajra\DataTables\Facades\DataTables::class,
-    // ],
-       
+          
 ];
