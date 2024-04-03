6b2225fbadbba0a0ece2ed095d1545ce45e32e6d
nikhil patel <nikhil.07patel@gmail.com>
2024-04-03 22:45:30 +0530

    image uploading and displaying by AJAX is done

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/ajax_upload', 'AjaxUploadController@index');
-// Route::post('/ajax_upload/action', 'AjaxUploadController@action')->name('ajaxupload.action');
- 
-
 Route::get('/ajax_upload', [AjaxUploadController::class, 'index']);
 Route::post('/ajax_upload/action', [AjaxUploadController::class, 'action'])->name('ajaxupload.action');
